4bbfc1bad5777fdb32f437acb05ef539add661cf
free3et <free3et@gmail.com>
2023-07-28 00:28:55 +0300

    add a function to add new notes

--- src/main.js
+++ src/main.js
@@ -7,13 +7,22 @@ import { deleteNote } from "./actionsOnNotes/deleteNote";
 import { archiveNote } from "./actionsOnNotes/archiveNote";
 import { unarchiveNote } from "./actionsOnNotes/unarchiveNote";
 
+const createNoteBtn = document.querySelector("#addNewNote");
+const newNoteForm = document.querySelector("#newNoteForm");
+
+function showAddNewNoteForm() {
+  newNoteForm.classList.toggle("show");
+  newNoteForm.classList.toggle("hide");
+  createNoteBtn.innerText = "Hide new note form";
+}
+
 // Display notes on initial page load
 window.addEventListener("load", showAllNotes);
 window.addEventListener("load", showSummaryTable);
+createNoteBtn.addEventListener("click", showAddNewNoteForm);
 
 // Handle form submission
 document.getElementById("newNoteForm").addEventListener("submit", (event) => {
   event.preventDefault();
   addNote();
-  editNote();
 });
